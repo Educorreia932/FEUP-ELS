d829db461c81ba7e3441538cbb35403ec26230b4
Educorreia932 <skelozard1@gmail.com>
2023-09-30 18:39:37 +0100

    Moved all config fields classes to separate files

---
+++ src/pt/up/fe/els2023/config/fields/Config.java
@@ -0,0 +1,11 @@
+package pt.up.fe.els2023.config.fields;
+
+import pt.up.fe.els2023.config.fields.commands.SelectField;
+
+import java.util.List;
+
+public class Config {
+    public List<FileField> source;
+    public List<SelectField> commands;
+    public List<FileField> target;
+}
