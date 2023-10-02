e2b656626a0109c61f7fa6cd4af3421ce8c7784b
Educorreia932 <skelozard1@gmail.com>
2023-10-02 23:05:13 +0100

    Changed FileData to a Record

--- src/pt/up/fe/els2023/model/FileData.java
+++ src/pt/up/fe/els2023/model/FileData.java
@@ -2,20 +2,5 @@ package pt.up.fe.els2023.model;
 
 import java.util.Map;
 
-public class FileData {
-    private Map<String, Object> content;
-    private String name;
-
-    public FileData(Map<String, Object> content, String name) {
-        this.content = content;
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Map<String, Object> getContent() {
-        return content;
-    }
+public record FileData(Map<String, Object> contents, String name) {
 }
