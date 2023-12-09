c4c0fa5335a326d0130ea98a5c5cea1edcb477f4
Eduardo Correia <skelozard1@gmail.com>
2023-12-09 12:30:50 +0000

    Implemented parsing of some operations

--- src/pt/up/fe/els2023/Parser.java
+++ src/pt/up/fe/els2023/Parser.java
@@ -9,12 +9,15 @@ import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.xtext.parser.IParser;
 
 import org.xtext.example.tablefork.TableForkStandaloneSetup;
-import org.xtext.example.tablefork.tableFork.Load;
-import org.xtext.example.tablefork.tableFork.Program;
+import org.xtext.example.tablefork.tableFork.*;
 import org.xtext.example.tablefork.tableFork.impl.ProgramImpl;
+import pt.up.fe.els2023.internal.Selection;
+import pt.up.fe.els2023.internal.TableInteraction;
+import pt.up.fe.els2023.model.table.Table;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 public class Parser {
     @Inject
@@ -39,14 +42,58 @@ public class Parser {
         }
 
         var treeIterator = resource.getAllContents();
+
+        TableInteraction tableInteraction = TableInteraction.fromTable(new Table());
         
         while (treeIterator.hasNext()) {
             var element = treeIterator.next();
 
-            if (element instanceof Program program) {
-                System.out.println(program.getLoad());
-                System.out.println(program.getOperations());
+            if (element instanceof Load load) {
+                String path = removeQuotes(load.getPath());
+                tableInteraction = TableInteraction.load(path);
+            }
+
+            if (element instanceof Select select) {
+                var fields = select.getFields();
+                var froms = select.getFrom();
+                
+                Selection selection = tableInteraction.select();
+                
+                for (var field : fields) {
+                    var fieldNames = field.getFieldNames().stream().map(this::removeQuotes).toList();
+
+                    selection = selection
+                        .fields(fieldNames.toArray(String[]::new));
+                }
+                
+                for (var from : froms) {
+                    String fromField = removeQuotes(from.getFromField());
+
+                    var fieldNames = from.getFields().getFieldNames().stream().map(this::removeQuotes).toList();
+
+                    selection = selection
+                        .from(fromField)
+                            .fields(fieldNames.toArray(String[]::new))
+                        .end();
+                }
+                
+                tableInteraction = selection.end();
+            }
+
+            if (element instanceof Rename rename) {
+                String fieldName = removeQuotes(rename.getFieldName());
+                String newName = removeQuotes(rename.getNewName());
+                
+                tableInteraction.rename(fieldName, newName);
+            }
+
+            if (element instanceof Save program) {
+                tableInteraction.save("result.csv");
             }
         }
+    }
+    
+    private String removeQuotes(String string) {
+        return string.substring(1, string.length() - 1);
     }
 }
